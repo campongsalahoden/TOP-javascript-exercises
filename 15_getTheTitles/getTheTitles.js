445caa64b270db294af7ab26873e000ed53bba82
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 02:32:45 +0800

    GetTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
-
-// Do not edit below this line
-module.exports = getTheTitles;
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
+  };
+  
+  // Do not edit below this line
+  module.exports = getTheTitles;
+  

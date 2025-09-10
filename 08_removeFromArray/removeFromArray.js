ecfe66a3690333cd9d73c3b2f6fad0bf25a000f4
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 01:37:51 +0800

    RemoveFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter(item => !valuesToRemove.includes(item));
+  };
+  
+  // Do not edit below this line
+  module.exports = removeFromArray;
+  

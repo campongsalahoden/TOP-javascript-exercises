b78fb5bb2ba240ad8ca64a9b7fadaa0ff7db77de
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 02:39:01 +0800

    FindTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+      const currentYear = new Date().getFullYear();
+  
+      const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+      const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+  
+      return currentAge > oldestAge ? current : oldest;
+    });
+  };
+  
+  // Do not edit below this line
+  module.exports = findTheOldest;
+  

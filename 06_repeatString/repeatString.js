eb4b5b697e8bcd8270d9cb34385467ec34e7bd08
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 00:47:52 +0800

    ERROR

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(str, num) {
+    
+    if (num < 0) return "ERROR";
+  
+    
+    let result = "";
+  
+    
+    for (let i = 0; i < num; i++) {
+      result += str;
+    }
+  
+    return result;
+  };
+  
+  // Do not edit below this line
+  module.exports = repeatString;
+  

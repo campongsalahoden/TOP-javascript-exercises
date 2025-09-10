afc6578d317078d654eb3f1fbb1fe09f030868a6
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 02:23:54 +0800

    Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (str) {
+   
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+   
+    return cleaned === cleaned.split("").reverse().join("");
+  };
+  
+  // Do not edit below this line
+  module.exports = palindromes;
+  

1d41b9e98a54b2aacaeb06ae997abc2188e97efd
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 01:56:13 +0800

    LeapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+const leapYears = function(year) {
+   
+    if (year % 400 === 0) {
+      return true;
+    }
+   
+    if (year % 100 === 0) {
+      return false;
+    }
+   
+    if (year % 4 === 0) {
+      return true;
+    }
+   
+    return false;
+  };
+  
+  // Do not edit below this line
+  module.exports = leapYears;
+  

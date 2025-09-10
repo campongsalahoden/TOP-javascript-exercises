180b60619d231b8ab15f3a5be10b10a81a0fb79f
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 02:28:59 +0800

    Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
-};
-
-// Do not edit below this line
-module.exports = fibonacci;
+const fibonacci = function(n) {
+    
+    n = Number(n);
+  
+    
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+  
+    
+    if (n === 1 || n === 2) return 1;
+  
+    
+    let a = 1, b = 1;
+    for (let i = 3; i <= n; i++) {
+      const next = a + b;
+      a = b;
+      b = next;
+    }
+    return b;
+  };
+  
+  // Do not edit below this line
+  module.exports = fibonacci;
+  

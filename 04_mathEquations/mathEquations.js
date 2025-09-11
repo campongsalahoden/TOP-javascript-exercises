32a542a8f74053aa94d581f542d270e7cd995a15
campongsalahoden <campongsalahoden@gmail.com>
2025-09-11 17:40:27 +0800

    MathEquations

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8
 const c = 22 * 3
-const d = 1
+const d = 6 % 5 
 const e = b - 17
-const f = 68
+const f = a + b + c + d + e
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
